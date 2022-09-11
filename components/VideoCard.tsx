c0f80db5f0dbc00ea5fd72f203ac35941ce99013
kelvin beno <kelvinbeno526@gmail.com>
2022-09-11 09:18:17 +0300

    styled video card

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,18 +1,136 @@
-import React from 'react'
-import {Video} from '../types'
-import {NextPage} from 'next'
+import React, { useState, useEffect, useRef } from "react";
+import { Video } from "../types";
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
 
-
-interface Iprops{
-    post: Video
+interface Iprops {
+  post: Video;
 }
 
-const VideoCard: NextPage<Iprops> = () => {
+const VideoCard: NextPage<Iprops> = ({ post }) => {
+
+  const [isHover, setIsHover] = useState(false)
+  const [playing, setIsplaying] = useState(false)
+  const [isVideoMuted, setIsVideoMuted] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const onVideoPress = () => {
+
+    if(playing){
+      videoRef?.current?.pause()
+      setIsplaying(false)
+    }else{
+      videoRef?.current?.play()
+      setIsplaying(true)
+
+    }
+
+  }
+
+
+  const onVolumePress = () => {
+
+    console.log('i was hit')
+    if(isVideoMuted){
+      setIsVideoMuted(false)
+    }else{
+      setIsVideoMuted(true)
+    }
+  }
+
+  console.log('isVideoMuted', isVideoMuted)
+
+
+
+
+
   return (
-    <div>VideoCard</div>
-  )
-}
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 item-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName}
+                  {`
+                    `}
+                </p>
+                <GoVerified className="text-blue-400 text-md" />
 
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
 
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div onMouseEnter={() => {setIsHover(true)}} onMouseLeave={() => {setIsHover(false)}} className="rounded-3xl">
+          <Link href="/">
+            <video
+            ref={videoRef}
+              src={post.video.asset.url}
+              loop
+              muted={isVideoMuted}
+              className="lg:w[600px] h-[300px] md:h-400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+            ></video>
+          </Link>
+          {isHover && (
 
-export default VideoCard
+            <div className="absolute
+            bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg: justify-between w-[100px] md:w-[50px] p-3
+            ">
+              {
+                playing ? (
+                  <button onClick={onVideoPress}>
+                    <BsFillPauseFill className="text-black text-2xl lg:text-4xl"/>
+                  </button>
+                ):
+                (
+                  <button onClick={onVideoPress}>
+                    <BsFillPlayFill className="text-black text-2xl lg:text-4xl"/>
+                  </button>
+                )
+              }
+              {
+                isVideoMuted ? (
+                  <button onClick={onVolumePress}>
+                    <HiVolumeOff className="text-black text-2xl lg:text-4xl"/>
+                  </button>
+                ):
+                (
+                  <button onClick={onVolumePress}>
+                    <HiVolumeUp className="text-black text-2xl lg:text-4xl"/>
+                  </button>
+                )
+              }
+
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
